5de1df38b383aeb22aa9ff8dc71de1440007c35e
imac1901 <isidoramack@gmail.com>
2019-01-18 13:10:44 -0600

    Initial commit

---
+++ app/src/main/java/Fragments.java
@@ -0,0 +1,6 @@
+public class Fragments extends android.support.v4.app.Fragment{
+
+
+
+
+}
---
+++ app/src/main/java/familyMember1.java
@@ -0,0 +1,57 @@
+public class familyMember {
+
+String firstName;
+String lastName;
+String personalityColor;
+String zodiacSign;
+String favMeal;
+String favSeason;
+
+    public String getPersonalityColor() {
+        return personalityColor;
+    }
+
+    public void setPersonalityColor(String personalityColor) {
+        this.personalityColor = personalityColor;
+    }
+
+    public String getZodiacSign() {
+        return zodiacSign;
+    }
+
+    public void setZodiacSign(String zodiacSign) {
+        this.zodiacSign = zodiacSign;
+    }
+
+    public String getFavMeal() {
+        return favMeal;
+    }
+
+    public void setFavMeal(String favMeal) {
+        this.favMeal = favMeal;
+    }
+
+    public String getFavSeason() {
+        return favSeason;
+    }
+
+    public void setFavSeason(String favSeason) {
+        this.favSeason = favSeason;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
